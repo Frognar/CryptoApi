331399927c0e8d5a2eed4d8ed862b89166344e6f
frognar <sebastian.przyszlak@gmail.com>
2023-12-16 18:21:56 +0100

    Update docker-run script

--- docker-run.bat
+++ docker-run.bat
@@ -1,2 +1,4 @@
-docker build -t crypto .
+docker stop crypto
+docker rm crypto
+docker build -t crypto:latest .
 docker run -d --restart unless-stopped -p 8081:8081 -p 8080:8080 --name crypto crypto
